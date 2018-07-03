02ad8dfb7b5df6a53ed928286a49ef8b554eb18b
Dawid Goc <dg@quantavi.com>
2018-07-03 11:36:03 +0200

    wysiwyg image upload from url

---
+++ customs/interfaces/custom_wysiwyg_full/interface.js
@@ -0,0 +1,319 @@
+/* global $ */
+define([
+  'app',
+  'utils',
+  'underscore',
+  'core/CustomUIView',
+  'tinyMCE',
+  'core/notification'
+], function (app, Utils, _, UIView, tinyMCE, Notification) {
+  'use strict';
+
+  return UIView.extend({
+    template: 'custom_wysiwyg_full/input',
+
+    serialize: function () {
+      var value = this.options.value || '';
+      var statusMapping = app.statusMapping.get('*').toJSON().mapping.toJSON();
+      var status = this.options.model.attributes.status || 1;
+
+      return {
+        value: value,
+        readOnly: this.options.settings.get('read_only') || !this.options.canWrite || statusMapping[status].read_only,
+        name: this.options.name
+      };
+    },
+
+    afterRender: function () {
+      var self = this;
+      var settings = this.options.settings;
+      var elementpath = Boolean(Number(settings.get('show_element_path')));
+      var styleFormats = []; // Format menu (styleselect) options
+      var headings = getCheckboxesSettings('headings');
+      var inline = getCheckboxesSettings('inline');
+      var blocks = getCheckboxesSettings('blocks');
+      var alignment = getCheckboxesSettings('alignment');
+      var toolbarOptions = getCheckboxesSettings('toolbar_options');
+      var toolbar = '';
+
+      function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
+      if (headings.length > 0) {
+        styleFormats.push({
+          title: 'Headings',
+          items: headings.map(function (heading) {
+            return {
+              title: 'Heading ' + heading.slice(-1), // Heading + last char of h1 thru h6 = 1...6
+              format: heading
+            };
+          })
+        });
+      }
+
+      if (inline.length > 0) {
+        styleFormats.push({
+          title: 'Inline',
+          items: inline.map(function (option) {
+            return {
+              title: capitalizeFirstLetter(option),
+              icon: option,
+              format: option
+            };
+          })
+        });
+      }
+
+      if (blocks.length > 0) {
+        styleFormats.push({
+          title: 'Blocks',
+          items: blocks.map(function (option) {
+            return {
+              title: option === 'p' ? 'Paragraph' : capitalizeFirstLetter(option),
+              format: option
+            };
+          })
+        });
+      }
+
+      if (alignment.length > 0) {
+        styleFormats.push({
+          title: 'Alignment',
+          items: alignment.map(function (option) {
+            return {
+              title: capitalizeFirstLetter(option.substring(5)),
+              icon: option,
+              format: option
+            };
+          })
+        });
+      }
+
+      if  (styleFormats.length > 0 && this.options.settings.get('show_format_menu')) {
+        toolbar = 'styleselect ';
+      }
+
+      if (toolbarOptions.length > 0) {
+        // Convert inline / alignment to appropriate options & add to toolbar
+        toolbar += toolbarOptions
+          .map(function (option) {
+            if (option === 'inline' && inline.length > 0) {
+              return inline.reduce(function (inlineStr, inlineOption) {
+                return inlineStr + ' ' + inlineOption;
+              });
+            } else if (option === 'alignment' && alignment.length > 0) {
+              return alignment.reduce(function (alignmentStr, alignmentOption) {
+                return alignmentStr + ' ' + alignmentOption;
+              });
+            }
+            return option;
+          })
+          .reduce(function (str, option) {
+            return str + ' ' + option;
+          });
+      }
+
+      toolbar += settings.get('custom_toolbar_options');
+
+      // Parse custom_wrapper and add to toolbar
+      var customWrapperSettings;
+      if (settings.get('custom_wrapper').length > 0) {
+        try {
+          customWrapperSettings = JSON.parse(settings.get('custom_wrapper'));
+        } catch (err) {
+          console.error(err);
+        }
+
+        toolbar += ' | ' + Object.keys(customWrapperSettings).join(' ');
+      }
+
+      var options = {
+        plugins: 'table hr lists link image print pagebreak code insertdatetime media autoresize paste preview',
+        selector: '#wysiwyg_' + this.options.name,
+        branding: false,
+        skin: 'directus',
+        autoresize_max_height: this.options.settings.get('max_height'),
+        elementpath: elementpath,
+        statusbar: elementpath,
+        convert_urls: false,
+        menubar: false,
+        readonly: this.options.settings.get('read_only') || !this.options.canWrite,
+        toolbar: toolbar,
+        content_style: 'body.mce-content-body {font-family: \'Roboto\', sans-serif;line-height:24px;letter-spacing:0.2px;font-size:14px;color:#333;margin:0;padding: 10px 30px !important;-webkit-font-smoothing:antialiased;text-rendering:optimizeLegibility;}body.mce-content-body p{line-height:inherit !important;}body.mce-content-body img{max-width:100% !important;}',
+        style_formats: styleFormats,
+        file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            
+            // Note: In modern browsers input[type="file"] is functional without 
+            // even adding it to the DOM, but that might not be the case in some older
+            // or quirky browsers like IE, so you might want to add it to the DOM
+            // just in case, and visually hide it. And do not forget do remove it
+            // once you do not need it anymore.
+
+            input.onchange = function() {
+              var file = this.files[0];
+              
+              var reader = new FileReader();
+              reader.onload = function () {
+                // Note: Now we need to register the blob in TinyMCEs image blob
+                // registry. In the next release this part hopefully won't be
+                // necessary, as we are looking to handle it internally.
+                var id = 'blobid' + (new Date()).getTime();
+                var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                var base64 = reader.result.split(',')[1];
+                var blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+
+                // call the callback and populate the Title field with the file name
+                cb(blobInfo.blobUri(), { title: file.name });
+              };
+              reader.readAsDataURL(file);
+            };
+            
+            input.click();
+        },
+        images_upload_handler: function (blobInfo, success, failure) {
+        	var data = {
+	        	"name": blobInfo.blob().name,
+	        	"title": blobInfo.blob().name.substring(0, blobInfo.blob().name.length-4),
+	        	"size": blobInfo.blob().size,
+	        	"type": blobInfo.blob().type,
+	        	"charset": "binary",
+	        	"data": `data:${blobInfo.blob().type};base64,${blobInfo.base64()}`,
+	        	"user": app.user.id
+    	    };
+        	var settings = {
+				"async": true,
+				"crossDomain": true,
+				"url": "http://localhost:8888/directus/api/1.1/files",
+				"method": "POST",
+				"data": data
+			};
+			$.ajax(settings)
+				.done(function (response) {
+					success(response.data.url);
+				})
+				.fail(function (response) {
+					console.warn(response);
+					Notification.warn("tinyMCE Image Upload Handler", response);
+				});
+        },
+        setup: function (editor) {
+          /**
+           * Event handler for the input event of the TineMCE editor
+           *
+           * Is needed because we need to set the value of the parent model onInput / onChange
+           * Normally, we'd do that using Backbone events, but since we're relying
+           * on TinyMCE for data creation, we need to use TinyMCEs event system in this instance
+           */
+          var saveEditorContents = _.debounce(function () {
+            if (editor && editor.getContent) {
+              var content = editor.getContent();
+              var value = self.model.get(self.name);
+
+              if ((Utils.isNothing(value) || value !== content)) {
+                self.model.set(self.name, content);
+                self.$el.find('textarea')[0].innerHTML = content;
+              }
+            }
+          }, 500);
+
+          var onEditorInit = function () {
+            self.$body = $(editor.getBody());
+          };
+
+          editor.on('init', onEditorInit);
+
+          // All events on which the contents of the editor should be saved
+          editor.on('input', saveEditorContents);
+          editor.on('ExecCommand', saveEditorContents);
+          editor.on('blur', saveEditorContents);
+          editor.on('paste', saveEditorContents);
+          editor.on('undo', saveEditorContents);
+          editor.on('redo', saveEditorContents);
+          editor.on('NodeChange', saveEditorContents);
+
+          if (customWrapperSettings) {
+            var previewStyles = '';
+
+            Object.keys(customWrapperSettings).forEach(function (identifier) {
+              // Add preview styling if set
+              if (customWrapperSettings[identifier].selector && customWrapperSettings[identifier].preview_style) {
+                previewStyles += customWrapperSettings[identifier].selector + ' {' + customWrapperSettings[identifier].preview_style + '}\n';
+              }
+
+              // Add button to editor
+              editor.addButton(identifier, {
+                title: customWrapperSettings[identifier].name,
+                text: customWrapperSettings[identifier].label || customWrapperSettings[identifier].name.match(/\b(\w)/g).join('').toUpperCase(),
+                onclick: function () {
+                  var text = editor.selection.getContent({format: 'text'});
+                  if (text && text.length > 0) {
+                    editor.execCommand('mceInsertContent', false, customWrapperSettings[identifier].template.replace(/{{text}}/g, text));
+                  }
+                }
+              });
+            });
+
+            previewStyles = String(previewStyles);
+
+            // This = tinyMCE instance
+            this.contentStyles.push(previewStyles);
+          }
+
+          // Add focus styles
+          editor.on('focus', function (e) {
+            e.target.editorContainer.classList.add('focus');
+          });
+
+          editor.on('blur', function (e) {
+            e.target.editorContainer.classList.remove('focus');
+          });
+        }
+      };
+
+      if (this.options.settings.get('remove_unsafe_tags') === false) {
+        options.valid_elements = '*[*]';
+        options.extended_valid_elements = '*[*]';
+      }
+
+      if (settings.get('basic_image_list')) {
+        options.image_list = function(success) {
+          var collection = app.files;
+          collection.once('sync', function(collection) {
+            var images = [];
+            collection.each(function(model) {
+              if (model.get('type').match(/image/g)) {
+                images.push({
+                  'text': model.get('title'),
+                  'value': model.get('url')
+                });
+              }
+            });
+            success(images)
+          })
+          collection.fetch();
+        }
+      }
+
+      tinyMCE.init(options);
+
+      function getCheckboxesSettings(name) {
+        return settings
+          .get(name)
+          .split(',')
+          .filter(function (option) {
+            return option.length > 0;
+          });
+      }
+    },
+
+    cleanup: function () {
+      // Remove tinyMCE
+      tinyMCE.remove();
+    }
+  });
+});
