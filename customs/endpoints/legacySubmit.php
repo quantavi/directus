98f7c0fd3a55fc640148713aa8adbc8c6dc9703d
Dawid Goc <dg@quantavi.com>
2018-08-13 15:49:41 +0200

    fix 5

--- customs/endpoints/legacySubmit.php
+++ customs/endpoints/legacySubmit.php
@@ -15,6 +15,7 @@ $childs_inheriting = [];
 $childs_data = [];
 $childs = [];
 $stack = [];
+$fuck_it = [];
 
 function getItem($table, $id = null, $token, $args = null) {
     global $server_url;
@@ -58,25 +59,54 @@ function getPosition($table, $token) {
 }
 
 function submitLegacy($root_item, $root_position, $data) {
-    global $childs_inheriting, $childs_data, $childs, $ignore, $stack;
+    global $childs_inheriting, $childs_data, $childs, $ignore, $stack, $fuck_it;
     // Debug 0
     if ($data->debug) array_push($stack, $root_item->data->image);
-    
-    if (is_object($root_item) && is_object($root_item->data)) {
-        // 0
-        foreach ($root_item->data as $child) {
-            // 1
-            if (is_object($child)) {
-                // Debug 1
-                if ($data->debug) array_push($childs_inheriting, $child);
-                // If data of the child is an object
-                if (is_object($child->data)) {
-                    // Child data
-                    $id = $child->data->id;
-                    $status = $child->data->status;
-                    $table = $child->meta->table;
-                    $position = getPosition($table, $data->token);
-                    // 2
+    // 0
+    foreach ($root_item->data as $child) {
+        // 1
+        if (is_object($child)) {
+            // Debug 1
+            if ($data->debug) array_push($childs_inheriting, $child);
+            // If data of the child is an object
+            if (is_object($child->data)) {
+                // Child data
+                $id = $child->data->id;
+                $status = $child->data->status;
+                $table = $child->meta->table;
+                $position = getPosition($table, $data->token);
+                // 2
+                if ($position > $root_position && $status != $data->status && !in_array($table, $ignore)) {
+                    // Debug 2
+                    if ($data->debug) {
+                        array_push($childs_data, [
+                            'id' => $id,
+                            'status' => $status,
+                            'table' => $table,
+                            'position' => $position
+                        ]);
+                    }
+                    // Data to update
+                    $update = [
+                        'status' => $data->status
+                    ];
+                    // Update child and get their childs
+                    $itsChilds = json_decode(updateItem($table, $id, $update, $data->token));
+                    // Debug 3
+                    array_push($childs, $itsChilds);
+                    // Repeat or die
+                    submitLegacy($itsChilds, $position, $data);
+                }
+            } else {
+                // Global data
+                $table = $child->meta->table;
+                $position = getPosition($table, $data->token);
+                // 2`
+                foreach ($child->data as $item) {
+                    // Item data
+                    $id = $item->id;
+                    $status = $item ->status;
+                    // 3
                     if ($position > $root_position && $status != $data->status && !in_array($table, $ignore)) {
                         // Debug 2
                         if ($data->debug) {
@@ -98,46 +128,16 @@ function submitLegacy($root_item, $root_position, $data) {
                         // Repeat or die
                         submitLegacy($itsChilds, $position, $data);
                     }
-                } else {
-                    // Global data
-                    $table = $child->meta->table;
-                    $position = getPosition($table, $data->token);
-                    // 2`
-                    foreach ($child->data as $item) {
-                        // Item data
-                        $id = $item->id;
-                        $status = $item ->status;
-                        // 3
-                        if ($position > $root_position && $status != $data->status && !in_array($table, $ignore)) {
-                            // Debug 2
-                            if ($data->debug) {
-                                array_push($childs_data, [
-                                    'id' => $id,
-                                    'status' => $status,
-                                    'table' => $table,
-                                    'position' => $position
-                                ]);
-                            }
-                            // Data to update
-                            $update = [
-                                'status' => $data->status
-                            ];
-                            // Update child and get their childs
-                            $itsChilds = json_decode(updateItem($table, $id, $update, $data->token));
-                            // Debug 3
-                            array_push($childs, $itsChilds);
-                            // Repeat or die
-                            submitLegacy($itsChilds, $position, $data);
-                        }
-                    }
                 }
             }
+        } else {
+            array_push($fuck_it, $child);
         }
     }
 }
 
 $app->post('/legacySubmit', function () {
-    global $server_url, $childs_inheriting, $childs_data, $childs, $ignore, $stack;
+    global $server_url, $childs_inheriting, $childs_data, $childs, $ignore, $stack, $fuck_it;
     // Get root data
     $data = json_decode($_POST['data']);
     // Get root table, id, position and token
@@ -151,7 +151,7 @@ $app->post('/legacySubmit', function () {
     // Get root item
     $root_item = json_decode(getItem($root_table, $root_id, $token)); 
     // Transfer the status to all children recursively
-//     submitLegacy($root_item, $root_position, $data);
+    submitLegacy($root_item, $root_position, $data);
     // If debug mode is enabled return more data about what's goind on
     if ($data->debug) {
         // JSON response
@@ -162,7 +162,8 @@ $app->post('/legacySubmit', function () {
             'childs_updated' => $childs,
             'childs_inheriting_data' => $childs_data,
             'childs_available' => $childs_inheriting,
-            'stack' => $stack
+            'stack' => $stack,
+            'fuck_it' => $fuck_it
         ]);
     } else {
         // JSON response
