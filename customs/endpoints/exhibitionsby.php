18ce879e8fc857fab35d0441ddcf1991f3a6265e
Dawid Goc <dg@quantavi.com>
2018-08-17 13:34:11 +0200

    endpoint with depth(0,1), by museum(id), by id(exhibition id)

--- customs/endpoints/exhibitionsby.php
+++ customs/endpoints/exhibitionsby.php
@@ -7,31 +7,55 @@ use Directus\View\JsonView;
 // Slim App
 $app = Bootstrap::get('app');
 
-// Simple GET endpoint example
-$app->get('/exhibitionsby', function () use ($app) {
-    // GET params
-    $museum_id = $app->request()->params('museum');
-    $depth = $app->request()->params('depth') || 0;
-    $debug = $app->request()->params('debug') || 0;
-    
-    // Junction Table connection
-    $junctionTable = TableGatewayFactory::create('junction_exhibition_to_museum');
-    $junctionTableData = $junctionTable->getItems([
-        'museum' => $museum_id
-    ])['data'];
-    
-    // Exhibitions Table connection
-    $exhibitionsTable = TableGatewayFactory::create('exhibitions');
-    
-    // Directus Files Table connection
-    $directusFilesTable = TableGatewayFactory::create('directus_files');
-    
-    // Arrays
-    $filteredJunctionTableData = [];
-    $relatedTablesIds = [];
-    $relatedTables = [];
-    
-    // Get root_url & root_ratios_url
+// Debug Table
+$debugTable = [];
+
+function debugAdd($name, $table) {
+    global $debugTable;
+    array_push($debugTable, $table);
+}
+
+function getTableItems($table, $params = [], $group = 'data') {
+    return TableGatewayFactory::create($table)->getItems($params)[$group];
+}
+
+function filterBy($rawData, $compare, $to, $return = null) {
+    $filtered = [];
+    foreach($rawData as $data) {
+        if ($data[$compare] == $to) {
+            array_push($filtered, $return ? $data[$return] : $data);
+        }
+    }
+    return $filtered;
+}
+
+function getFilteredItems($tableToParse, $compare, $to, $connectBy = null, $additionalTable) {
+    $filteredItemsArray = [];
+    $filteredItems = filterBy($tableToParse, $compare, $to, $connectBy);
+    foreach ($filteredItems as $item) {
+        $item = getItemBy($additionalTable, $connectBy != null ? $connectBy : 'id', $item);
+        if ($item['photo']) {
+            $item['photo'] = addImagesDataIfExistTo(getFileBy('id', $item['photo']));
+        }
+        array_push($filteredItemsArray, $item);
+    }
+    return $filteredItemsArray;
+}
+
+function getExhibitionBy($field, $value) {
+    return TableGatewayFactory::create('exhibitions')->findOneBy($field, $value); 
+}
+
+function getItemBy($table, $field, $value) {
+    return TableGatewayFactory::create($table)->findOneBy($field, $value);
+}
+
+function getFileBy($field, $value) {
+    return TableGatewayFactory::create('directus_files')->findOneBy($field, $value); 
+}
+
+function addImagesDataIfExistTo($object, $filenameField = null) {
+    // Get root_url & root_ratios_url 
     $config = Bootstrap::get('config');
     $fs = $config['filesystem'];
     $root_url = $fs['root_url'];
@@ -40,47 +64,90 @@ $app->get('/exhibitionsby', function () use ($app) {
     // Array with all ratios to check
     $r = ['sixteen_nine', 'four_three', 'three_two', 'one_one', 'free'];
     
-    // Compare id from junction entry to given museum id and get table with it if equals
-    foreach ($junctionTableData as $data) {
-        if ($data['museum'] == $museum_id) {
-            array_push($filteredJunctionTableData, $data);
-            array_push($relatedTablesIds, $data['id']);
-            
-            $entry = $exhibitionsTable->findOneBy('id', $data['id']);
-            
-            $image = $directusFilesTable->findOneBy('id', $entry['image']);
-            
-            $name = $image['name'];
-            $image['url'] = "$root_url/$name";
-            
-            for($j = 0; $j < count($r); $j++) {
-
-                $file = glob( $root_ratios_url.'/'.$r[ $j ].'/'.substr($name, 0, -4).'*' );
-                foreach( $file as $filefound ) {
-                    $image[ $r[ $j ].'_url' ] = str_replace( '/var/www/html', '', $filefound );
-                }
-                
-            }
-            
-            $entry['image'] = $image;
-            
-            array_push($relatedTables, $entry);
+    $name = $filenameField ? $object[$filenameField] : $object['name'];
+    $object['url'] = "$root_url/$name";
+    
+    for($j = 0; $j < count($r); $j++) {
+        
+        $file = glob( $root_ratios_url.'/'.$r[ $j ].'/'.substr($name, 0, -4).'*' );
+        foreach( $file as $filefound ) {
+            $object[ $r[ $j ].'_url' ] = str_replace( '/var/www/html', '', $filefound );
         }
+        
     }
     
-    // Add Descriptions and Authors
-    if ($depth >= 1) {
+    return $object;
+}
+
+// Simple GET endpoint example
+$app->get('/exhibitionsby', function () use ($app) {
+    global $debugTable;
+    
+    // GET params
+    $museum_id = $app->request()->params('museum');
+    $exhibition_id = $app->request()->params('id');
+    $depth = $app->request()->params('depth') || 0;
+    $debug = $app->request()->params('debug') || 0;
+    
+    if ($museum_id && $exhibition_id) {
+        return JsonView::render([
+            'message' => 'It\'s impossible to return exhibitions by museum id and single exhibition in the same time.'
+        ]);
+    }
+
+    $junctionTableData = getTableItems('junction_exhibition_to_museum');
+    
+    // Arrays
+    $filteredJunctionTableData = [];
+    $relatedTablesIds = [];
+    $relatedTables = [];
+    
+    if ($museum_id && !$exhibition_id) {
+        // Compare id from junction entry to given museum id and get table with it if equals
+        foreach ($junctionTableData as $data) {
+            if ($data['museum'] == $museum_id) {
+                array_push($filteredJunctionTableData, $data);
+                array_push($relatedTablesIds, $data['id']);
+                
+                $entry = getExhibitionBy('id', $data['id']);
+                $entry['image'] = addImagesDataIfExistTo(getFileBy('id', $entry['image']));
+                
+                // Add Descriptions and Authors
+                if ($depth >= 1) {
+                    // Add Descriptions
+                    $translationsTable = getTableItems('exhibition_translations');
+                    $entry['translations'] = filterBy($translationsTable, 'exhibition', $entry['id']);
+                    
+                    // Add Authors
+                    $junctionAuthorsExhibitionsTable = getTableItems('junction_author_to_exhibition');
+                    $entry['authors'] = getFilteredItems($junctionAuthorsExhibitionsTable, 'exhibition', $entry['id'], 'id', 'authors');
+                }
+                
+                array_push($relatedTables, $entry);
+            }
+        }
+    } else if ($exhibition_id && !$museum_id) {
+        $entry = getExhibitionBy('id', $exhibition_id);
+        $entry['image'] = addImagesDataIfExistTo(getFileBy('id', $entry['image']));
         
+        // Add Descriptions and Authors
+        if ($depth >= 1) {
+            // Add Descriptions
+            $translationsTable = getTableItems('exhibition_translations');
+            $entry['translations'] = filterBy($translationsTable, 'exhibition', $entry['id']);
+            
+            // Add Authors
+            $junctionAuthorsExhibitionsTable = getTableItems('junction_author_to_exhibition');
+            $entry['authors'] = getFilteredItems($junctionAuthorsExhibitionsTable, 'exhibition', $entry['id'], 'id', 'authors');
+        }
+        
+        array_push($relatedTables, $entry);
     }
     
     // Return
     if ($debug) {
         return JsonView::render([
-            'GET params' => $_GET,
-            'museum' => $museum_id,
-            'related tables ids' => $relatedTablesIds,
-            'junction' => $junctionTableData,
-            'filtered' => $filteredJunctionTableData,
+            'debug' => $debugTable,
             'data' => $relatedTables
         ]);
     } else {
